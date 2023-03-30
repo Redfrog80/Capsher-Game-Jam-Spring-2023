890190566ea9d94c2094301568a5375acbfc8a4c
Redfrog80 <90211112+Redfrog80@users.noreply.github.com>
2023-03-30 14:05:57 -0500

    Tilemap actually works now lol TODO get a method for collecting colliding object pairs into a dictionary. Implement actual physics/mass system Soft boundary around world. Add hasCollided(self, other) to gameObjects

--- src/gameWorld.py
+++ src/gameWorld.py
@@ -1,23 +1,25 @@
-from pygame import image, surface, transform
+from turtle import st
+from pygame import surface, Rect, font
 from lib.ImageDict import ImageDict
-from lib.misc import floorElementDiv
+from lib.misc import *
 from lib.objects import Camera, GameObject
 
 class gameWorld:
     def __init__(self, dimensions : tuple, tiledim : tuple, camera : Camera) -> None:
         self.__dim__ = dimensions # two tuples, top left cord, bottom right cord
-        self.__tiles__ = tiledim
+        self.__tiledim__ = tiledim
         self.__camera__ = camera
         self.__game_objects__ = {}
         self.tileMap = {} 
     
     def __update_tile_map__(self):
-        del self.tileMap
         self.tileMap = {}
         for key in self.__game_objects__:
-            for tup in [floorElementDiv(i,self.__tiles__) for i in self.__game_objects__[key].get_boundary_corners()]:
-                self.tileMap[tup] = [key] + self.tileMap.get(tup,[])
-        print(self.tileMap)
+            boundary = self.__game_objects__[key].boundary
+            topLeft, bottomRight = [floorElementDiv(i,self.__tiledim__) for i in [boundary.topleft, boundary.bottomright]]
+            for tup in [(i,j) for i in range(topLeft[0],bottomRight[0]+1) for j in range(topLeft[1],bottomRight[1]+1)]:
+                old = self.tileMap.get(tup,[])
+                self.tileMap[tup] = [key] + old
     
     def add_game_object(self, key : str, object : GameObject):
         self.__game_objects__[key] = object
@@ -32,14 +34,26 @@ class gameWorld:
             obj = self.__game_objects__[key]
             obj.update(dt)
             for fun in args:
-                fun(dt = dt, key = key, object = obj)
-            self.__update_tile_map__()
-        # need to ensure the camera is not looking outside playable area (so things don't seem to pop into existance)
-        # check for collisions
-        # perform collisions
+                fun(self = self, dt = dt, key = key, object = obj)
+
+        # Primative collision
+        self.__update_tile_map__()
+        for key in self.tileMap:
+            if len(self.tileMap[key]) > 1:
+                for key_a in self.tileMap[key]:
+                    for key_b in self.tileMap[key]:
+                        if key_a!=key_b and Rect.colliderect(self.__game_objects__[key_a].boundary, self.__game_objects__[key_b].boundary):
+                            obj_a = self.__game_objects__[key_a]
+                            obj_b = self.__game_objects__[key_b]
+                            obj_a.vel = addTuple(obj_a.vel, mulElements(mulTuple(unitTuple(obj_a.boundary.center,obj_b.boundary.center), 10*dt), self.__tiledim__))
+                
     def render(self, screen : surface, *args):
         for key in self.__game_objects__:
             obj = self.__game_objects__[key]
             obj.render(screen, self.__camera__)
             for fun in args:
-                fun(key = key, object = obj)
+                fun(self = self, screen = screen, key = key, object = obj)
+    
+    def render_tile_map(self,screen):
+        for tile in self.tileMap:
+            screen.fill((tile[0]*4%200+55,55,tile[1]*4%200+55), Rect(subTuple(mulElements(tile,self.__tiledim__), self.__camera__.boundary.topleft),self.__tiledim__))
